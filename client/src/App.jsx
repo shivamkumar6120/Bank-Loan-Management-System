3055845ef7ab643cf04eb6c070b4eee69dde8d9e
Jaydeep020 <goodsuccess8871@gmail.com>
2025-11-27 23:16:40 +0530

    Login Page Added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,16 @@
+import { Route, Router } from "react-router-dom"
+import { ToastContainer } from "react-toastify"
+import { Home } from "./pages/Home"
 
 function App() {
  
   return (
-    <>
-     
-    </>
+    <div>
+     <Router>
+        <Route path="/" element={<Home/>}></Route>
+     </Router>
+     <ToastContainer/>
+    </div>
   )
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,10 @@
-
+import {BrowserRouter} from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
 )
