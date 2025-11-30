405fe797c2a5284b9112bd25e0b08fb00b2410b2
shivamkumar6120 <nirajshivam6120@gmail.com>
2025-11-30 20:08:57 +0530

    App.jsx updated, Starting page Home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,24 @@
 import { Navigate, Route, Routes } from "react-router-dom"
 import { ToastContainer } from "react-toastify"
-import { Home } from "./pages/Home"
-import { Login } from "./pages/Login"
-import { Signup } from "./pages/Signup"
+import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
+import { Signup } from "./pages/Signup";
 
 function App() {
- 
   return (
     <div>
-     <Routes>
-        <Route path="/" element={<Navigate to='/login'/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/register" element={<Signup/>}/>
-        <Route path="/home" element={<Home/>}>
+      <Routes>
+        {/* Home Page as default landing page */}
+        <Route path="/" element={<Home />} />
 
-        </Route>
-     </Routes>
-     <ToastContainer/>
+        {/* Auth Routes */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Signup />} />
+      </Routes>
+
+      <ToastContainer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
