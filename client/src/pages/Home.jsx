86199d31dc56e61014515b21205ac0937488ee6c
shivamkumar6120 <nirajshivam6120@gmail.com>
2025-11-30 20:19:59 +0530

    Added heroSection, whychoosesection, footer and navbar

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React from "react";
 import { Outlet } from 'react-router-dom'
-import { Navbar } from '../components/Navbar'
+import Navbar from "../components/Navbar";
+import HeroSection from "../components/HeroSection";
+import WhyChooseSection from "../components/WhyChooseSection";
+import Footer from "../components/Footer";
 
 export const Home = () => {
   return (
-    <div>
-        <Navbar/>
-        <Outlet></Outlet>
+    <div className="bg-gray-50 min-h-screen">
+
+      {/* Navbar */}
+      <Navbar />
+
+      {/* Hero Section */}
+      <HeroSection />
+
+      {/* Why Choose Section  */}
+      <WhyChooseSection />
+
+      <Footer />
     </div>
-  )
-}
+  );
+};
