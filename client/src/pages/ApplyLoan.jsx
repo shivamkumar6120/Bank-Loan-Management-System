bd064c7a7cfa015b4520f2e4625e6764493219c0
shivamkumar6120 <nirajshivam6120@gmail.com>
2025-12-07 10:43:36 +0530

    ApplyLoan page created (trail for inspection)

---
+++ client/src/pages/ApplyLoan.jsx
@@ -0,0 +1,7 @@
+export default function ApplyLoan() {
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <h1 className="text-3xl font-bold">Apply Loan Page Under Construction</h1>
+    </div>
+  );
+}
