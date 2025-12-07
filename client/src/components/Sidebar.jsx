cf0284945845b651105cd235c17d0656a4f3c3d7
shivamkumar6120 <nirajshivam6120@gmail.com>
2025-12-07 17:22:13 +0530

    notification link updated

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -12,7 +12,7 @@ export default function Sidebar() {
     { icon: <Home size={20} />, label: "Apply Loan", link: "/apply-loan" },
     { icon: <CreditCard size={20} />, label: "My Loans", link: "/dashboard" },
     { icon: <Wallet size={20} />, label: "EMI Payments", link: "/dashboard/emi-payments" }, 
-    { icon: <Bell size={20} />, label: "Notifications", link: "notification" },
+    { icon: <Bell size={20} />, label: "Notifications", link: "/notification" },
     { icon: <User size={20} />, label: "Profile & KYC", link: "#" },
   ];
 
