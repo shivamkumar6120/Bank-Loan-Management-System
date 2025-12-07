3646cab7d8d5dbb4cb79eb512d6d2acfbbec9bb0
shivamkumar6120 <nirajshivam6120@gmail.com>
2025-12-07 16:08:55 +0530

    emi-payment link issues fixed and bank name updated

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -11,14 +11,14 @@ export default function Sidebar() {
     { icon: <Home size={20} />, label: "Dashboard", link: "/dashboard" },
     { icon: <Home size={20} />, label: "Apply Loan", link: "/apply-loan" },
     { icon: <CreditCard size={20} />, label: "My Loans", link: "/dashboard" },
-    { icon: <Wallet size={20} />, label: "EMI Payments", link: "emi-payments" }, // link to be added >>>?
+    { icon: <Wallet size={20} />, label: "EMI Payments", link: "/dashboard/emi-payments" }, // link to be added >>>?
     { icon: <Bell size={20} />, label: "Notifications", link: "#" },
     { icon: <User size={20} />, label: "Profile & KYC", link: "#" },
   ];
 
   return (
     <div className="w-64 bg-white shadow-md min-h-screen p-6 flex flex-col">
-      <h2 className="text-xl font-bold text-[#0A4D68] mb-8">NeoBank Center</h2>
+      <h2 className="text-xl font-bold text-[#0A4D68] mb-8">CDAC LOAN BANK</h2>
 
       <nav className="flex-1 space-y-3">
         {menuItems.map((item, index) => (
